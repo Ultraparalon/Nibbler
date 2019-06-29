8e1f9e97154029e5acbb5193f7c9882921ebb290
yzavhoro <yzavhoro@student.unit.ua>
2019-06-29 16:04:20 +0300

    Nibbler header

--- includes/Nibbler.h
+++ includes/Nibbler.h
@@ -1,12 +1,45 @@
 #ifndef NIBBLER_H
 #define NIBBLER_H
 
-class Nibbler
+#include <vector>
+
+enum gameStates {
+	GAME,
+	MENU,
+	GAMEOVER
+}
+
+enum snakeDirections {
+	UP, 
+	DOWN,
+	RIGHT,
+	LEFT
+}
+
+typedef struct	cube {
+	int x;
+	int y;
+	float size;
+}				Cube;
+
+class Nibbler : public IDynamic
 {
-public:
+	int						width;
+	int 					height;
+	std::vector<Cube>		*Nibbler;
+	enum snakeDirections	direction;
+	enum gameStates			state;
+
 	Nibbler();
+
+public:
+	Nibbler(int, int);
 	~Nibbler();
-	
+
+	void run();
+	void switchState(enum gameStates);
+	void changeDirection(enum snakeDirections);
+
 };
 
 #endif
